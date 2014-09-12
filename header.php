fcc80a6f86bcf6a6c40a8b6da124175fbd4bcb63
Alexandre Plennevaux <alexandre@pixeline.be>
2014-09-12 22:52:21 +0200

    favicon and apple-touch-icons

--- header.php
+++ header.php
@@ -21,8 +21,26 @@
 		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
 		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
 		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon-152x152.png">
+		<meta name="apple-mobile-web-app-title" content="Tisse La Toile">
+		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicon-196x196.png" sizes="196x196">
+		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicon-160x160.png" sizes="160x160">
+		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicon-16x16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/images/favicon-32x32.png" sizes="32x32">
+		<meta name="msapplication-TileColor" content="#dddddd">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/mstile-144x144.png">
+		<meta name="application-name" content="Tisse La Toile">
+
 		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/library/images/favicon.ico">
 		<![endif]-->
 		<?php // or, set /favicon.ico for IE10 win ?>
 		<meta name="msapplication-TileColor" content="#f01d4f">
@@ -44,7 +62,7 @@
 			<nav role="navigation" id="main-menu">
 						<?php wp_nav_menu(array(
     					'container' => false,                           // remove nav container
-    					'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+    					'menu' => 'The Main Menu',  // nav name
     					'menu_class' => 'nav cf',               // adding custom nav class
     					'theme_location' => 'main-nav',                 // where it's located in the theme
     					'before' => '',                                 // before the menu
